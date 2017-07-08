8ab7a483100eb84e49ba6be4bcdbd06749c72c0e
coffeeitem <christopherolvera71@gmail.com>
2017-07-08 11:04:43 -0700

    Update chathandler.js

--- chathandler.js
+++ chathandler.js
@@ -1,5 +1,5 @@
 function myFunction() {
-	var x = document.getElementsByName("fname")[0].value;
+	var x = document.getElementsByName("name")[0].value;
     document.getElementById("user").innerHTML = x;
 	var y = document.getElementsByName("name")[1].value;
     document.getElementById("output").innerHTML = y;
